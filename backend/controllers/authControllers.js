86cbefbccb1aec2c0e0b7b34103ecb0af5527171
vkkarthikasree@gmail.com <vkkarthikasree@gmail.com>
2026-01-23 10:07:03 +0530

    Initial full project

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
